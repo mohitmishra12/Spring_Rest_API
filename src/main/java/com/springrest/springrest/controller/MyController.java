c58ac6d71a501c1c30bb83080faa5be107983900
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-23 17:49:51 +0530

    postmaping

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -2,8 +2,7 @@ package com.springrest.springrest.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.springrest.springrest.entity.Course;
 import com.springrest.springrest.services.CourseService;
 
@@ -16,5 +15,18 @@ public class MyController {
     @GetMapping("/courses")
     public List<Course> getCourses() {
         return this.courseService.getCourses();
+    }
+
+    @GetMapping("/courses/{courseId}")
+    public  Course getCourse(@PathVariable String courseId){
+        return this.courseService.getCourses(Long.parseLong(courseId));
+    }
+
+
+    @PostMapping("/courses")
+    public Course addCourse(@RequestBody Course course){
+
+        return  this.courseService.addCoures(course);
+
     }
 }
